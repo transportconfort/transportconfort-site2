43d1099844111dd6bcf848107944af816ab80312
transportconfort <contact@transportconfort.com>
2025-10-14 22:43:51 +0200

    Create debug-env.js

---
+++ netlify/functions/netlify/functions/debug-env.js
@@ -0,0 +1,9 @@
+exports.handler = async () => {
+  const k = process.env.STRIPE_SECRET_KEY || "";
+  const safe = k ? (k.startsWith("sk_live_") ? "sk_live_" : "sk_test_") + k.slice(8, 14) : "absente";
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: `STRIPE_SECRET_KEY chargée: ${safe}`
+  };
+};
