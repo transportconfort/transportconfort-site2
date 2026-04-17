033d2e87fc4e3c544f8f23aad054eb70bebfaa45
transportconfort <contact@transportconfort.com>
2026-04-17 09:10:13 +0200

    Update minCharge factor for upto 45 from 0.85 to 0.95

--- js/simulator.js
+++ js/simulator.js
@@ -267,7 +267,7 @@ if (km <= 10) {
 
   function minCharge(m) {
      const bands = [
-  { upto: 45, factor: 0.85 },
+  { upto: 45, factor: 0.95 },
   { upto: 90, factor: 0.75 },
   { upto: Infinity, factor: 0.65 }
     ];
